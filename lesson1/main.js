5637c42c75950ef56e07f3a2751ee067f219bbb1
YuuKiomaru <nastiamay.1654@gmail.com>
2022-11-01 12:52:28 +0200

    add new inf

---
+++ lesson1/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let hello='hello';
+console.log(hello);
+
+let owu='owu';
+console.log(owu);
+
+let com='com';
+console.log(com);
+
+let ua='ua';
+console.log(ua);
+
+let num1= 1;
+console.log(num1);
+
+let num2= 10;
+console.log(num2);
+
+let firstName = 'Anastasia';
+let middName = ' Anatoliivna';
+let lastName = ' Martinchik';
+console.log(firstName + middName + lastName);
+
+let a = 100;
+console.log(typeof 100);
+
+let b = '100';
+console.log(typeof '100');
+
+let c = true;
+console.log(typeof true);
+
+
+
+
+
+
+
+
+
+
