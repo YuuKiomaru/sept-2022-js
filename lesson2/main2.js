71cb4e3d02e2d33df7b5a2c74ca888fb300f0009
YuuKiomaru <nastiamay.1654@gmail.com>
2022-11-03 19:18:17 +0200

    create number

---
+++ lesson2/main2.js
@@ -0,0 +1,34 @@
+
+    let group = [365, true, 'user', false, 200, [10, 20, 30], 'runner', true, 'index', 10];
+    console.log(group);
+
+        let book1 = {title: 'The Little Prince', pageCount: 594, genre:'Novella'}
+        let book2 = {title: 'The Hobbit', pageCount: 675, genre: 'Fantasy'}
+        let book3 = {title: 'Dream of the Red Chamber', pageCount: 490, genre: 'Family saga'}
+
+        let book4 = {title: 'And Then There Were None', pageCount: 570, genre: 'Mystery'}
+        book4.author = { name: 'Agatha Christie', age: 85}
+        let book5 = {title: 'Harry Potter and the Philosophers Stone', pageCount: 678, genre: 'Fantasy'}
+        book5.author = {name: 'J. K. Rowling', age: 57}
+        let book6 = {title: 'A Tale of Two Cities' ,pageCount: 747, genre: 'Historical fiction'}
+        book6.author = {name: 'Charles Dickens', age: 58}
+
+    let users = [
+        name: 'Anna', username: 'Anna12', password: 12345,
+        name: 'Olya',  username: 'Frog123', password: 67890,
+        name: 'Nastia',  username: 'LedyNasia', password: 4605038,
+        name: 'Oleg', username: 'YoungCat', password: 486946,
+        name: 'Vasya', username: 'CoolBoy', password: 6839573,
+        name: 'Katya', username: 'Kitty35', password: 689863,
+        name: 'Vova', username: 'Vovavova88', password: 0738684,
+        name: 'Masha', username: 'MissA', password: 5629573,
+        name: 'Kolya', username: 'NikoKo23', password: 265835,
+        name: 'Kristina', username: 'Tina17', password: 7544949,
+    ]
+   console.log(user[3], user[6], user[9], user[12], user[15], user[18], user[21], user[24], user[27], user[30]);
+
+
+
+
+
+
